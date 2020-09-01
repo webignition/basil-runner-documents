913a9b595ecfd64b3856d50a96ec9353ae1ffc06
Jon Cram <webignition@gmail.com>
2020-09-01 13:09:35 +0100

    Create Step model

---
+++ src/Step.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilRunnerDocuments;
+
+class Step implements DocumentInterface
+{
+    private const TYPE = 'step';
+
+    private string $name;
+    private string $status;
+
+    /**
+     * @var DocumentInterface[]
+     */
+    private array $statements;
+
+    /**
+     * @var array<mixed>|null
+     */
+    private ?array $data;
+
+    /**
+     * @param string $name
+     * @param string $status
+     * @param DocumentInterface[] $statements
+     * @param array<mixed> $data
+     */
+    public function __construct(string $name, string $status, array $statements, array $data = null)
+    {
+        $this->name = $name;
+        $this->status = $status;
+        $this->statements = array_filter($statements, function ($item) {
+            return $item instanceof DocumentInterface;
+        });
+        $this->data = $data;
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    public function getData(): array
+    {
+        $data = [
+            'name' => $this->name,
+            'status' => $this->status,
+        ];
+
+        if (count($this->statements)) {
+            $statementData = [];
+
+            foreach ($this->statements as $statement) {
+                $statementData[] = $statement->getData();
+            }
+
+            $data['statements'] = $statementData;
+        }
+
+        if (null !== $this->data) {
+            $data['data'] = $this->data;
+        }
+
+        return $data;
+    }
+}
---
+++ tests/Unit/StepTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilRunnerDocuments\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use webignition\BasilRunnerDocuments\DocumentInterface;
+use webignition\BasilRunnerDocuments\Step;
+
+class StepTest extends TestCase
+{
+    /**
+     * @dataProvider getDataDataProvider
+     *
+     * @param Step $step
+     * @param array<mixed> $expectedData
+     */
+    public function testGetData(Step $step, array $expectedData)
+    {
+        self::assertSame($expectedData, $step->getData());
+    }
+
+    public function getDataDataProvider(): array
+    {
+        $statement1 = \Mockery::mock(DocumentInterface::class);
+        $statement1
+            ->shouldReceive('getData')
+            ->andReturn([
+                'content' => 'statement 1 mock content',
+            ]);
+
+        $statement2 = \Mockery::mock(DocumentInterface::class);
+        $statement2
+            ->shouldReceive('getData')
+            ->andReturn([
+                'content' => 'statement 2 mock content',
+            ]);
+
+        $statusPassed = 'passed';
+        $statusFailed = 'failed';
+
+        return [
+            'passed, single statement' => [
+                'step' => new Step(
+                    'passed, single statement',
+                    $statusPassed,
+                    [
+                        $statement1,
+                    ]
+                ),
+                'expectedData' => [
+                    'name' => 'passed, single statement',
+                    'status' => $statusPassed,
+                    'statements' => [
+                        [
+                            'content' => 'statement 1 mock content',
+                        ]
+                    ],
+                ],
+            ],
+            'passed, multiple statements' => [
+                'step' => new Step(
+                    'passed, single action, single assertion',
+                    $statusPassed,
+                    [
+                        $statement1,
+                        $statement2,
+                    ]
+                ),
+                'expectedData' => [
+                    'name' => 'passed, single action, single assertion',
+                    'status' => $statusPassed,
+                    'statements' => [
+                        [
+                            'content' => 'statement 1 mock content',
+                        ],
+                        [
+                            'content' => 'statement 2 mock content',
+                        ],
+                    ],
+                ],
+            ],
+            'failed, single statement' => [
+                'step' => new Step(
+                    'failed, single assertion',
+                    $statusFailed,
+                    [
+                        $statement1,
+                    ]
+                ),
+                'expectedData' => [
+                    'name' => 'failed, single assertion',
+                    'status' => $statusFailed,
+                    'statements' => [
+                        [
+                            'content' => 'statement 1 mock content',
+                        ],
+                    ],
+                ],
+            ],
+            'passed, single statement with data' => [
+                'step' => new Step(
+                    'passed, single is assertion with data',
+                    $statusPassed,
+                    [
+                        $statement1,
+                    ],
+                    [
+                        'expected_value' => 'literal',
+                    ]
+                ),
+                'expectedData' => [
+                    'name' => 'passed, single is assertion with data',
+                    'status' => $statusPassed,
+                    'statements' => [
+                        [
+                            'content' => 'statement 1 mock content',
+                        ],
+                    ],
+                    'data' => [
+                        'expected_value' => 'literal',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
